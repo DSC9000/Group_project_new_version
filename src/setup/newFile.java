66636338e77115cadc737a093897b16f32932277
Daniel S <devolution.sdc@gmail.com>
2021-10-05 17:00:43 -0500

    new file on the class

---
+++ src/setup/newFile.java
@@ -0,0 +1,11 @@
+package setup;
+
+import java.util.Scanner;
+
+public class newFile {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+    }
+}
